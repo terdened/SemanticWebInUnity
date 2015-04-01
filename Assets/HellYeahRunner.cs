cfcc1b87ba850722c770b46c727ae461be2a262e
terdened <terdened@mail.ru>
2015-04-01 13:14:34 +0600

    + Hello World! + Included external library

---
+++ Assets/HellYeahRunner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using ExternalLibrary;
+
+public class HellYeahRunner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var hellYeah = new ExternalLibrary.HellYeah();
+        Debug.Log(hellYeah.Get2Plus2());
+        Debug.Log(hellYeah.GetHellYeah());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ExternalLibrary/HellYeah.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExternalLibrary
+{
+    public class HellYeah
+    {
+        public HellYeah()
+        {
+            
+        }
+
+        public int Get2Plus2()
+        {
+            return 2 + 2;
+        }
+
+        public string GetHellYeah()
+        {
+            return "Hell yeah!";
+        }
+    }
+}
